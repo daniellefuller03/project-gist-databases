5f037792cfb74bb5166900f0865ed7ace1e8b04f
daniellefuller00 <daniellefuller00@gmail.com>
2019-06-05 14:45:32 +0000

    added solutions

--- gists_database/search.py
+++ gists_database/search.py
@@ -1,4 +1,18 @@
 from .models import Gist
 
+
 def search_gists(db_connection, **kwargs):
-    pass
+    search = "SELECT * FROM gists"
+    if 'github_id' in kwargs:
+        search = "SELECT * FROM gists WHERE github_id = :github_id"
+    elif 'created_at' in kwargs:
+        search = "SELECT * FROM gists WHERE datetime(created_at) == datetime(:created_at)"
+
+    r = []
+    cursor = db_connection.execute(search,kwargs)
+    for row in cursor:
+        r.append(Gist(row))
+    return r
+    
+    
+    
